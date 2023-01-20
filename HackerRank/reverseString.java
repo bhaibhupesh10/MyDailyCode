bfa8fad705dacaa6640d73d05fc9fd2755c35467
Bhupesh <bhaibhupesh10@gmail.com>
2023-01-20 21:38:23 +0530

    reverse string question

---
+++ HackerRank/reverseString.java
@@ -0,0 +1,21 @@
+package HackerRank;
+
+public class reverseString {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("Hello");
+        System.out.println(str);
+        for(int i=0;i<str.length()/2; i++){
+
+            int front = i;
+            int back = str.length()-1-i;
+
+            char frontChar = str.charAt(front);
+            char backChar = str.charAt(back);
+
+            str.setCharAt(front, backChar);
+            str.setCharAt(back, frontChar);
+
+        }
+            System.out.println(str);
+    }
+}
